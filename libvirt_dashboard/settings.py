36ff58d79a2257b3747eb963be0093d3493ac10c
Kairui Song <kasong@redhat.com>
2016-07-27 18:23:43 +0800

    Use postgresql as backend.

--- libvirt_dashboard/settings.py
+++ libvirt_dashboard/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'libvirt_dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -127,3 +131,9 @@ REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE': 100,
 }
+
+try:
+    from libvirt_dashboard.settings_instance import *
+except ImportError:
+    print "Instance setting not found."
+
