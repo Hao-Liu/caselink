d5845158fd2271a97dfa967dcf586b627f3a2d80
Kairui Song <kasong@redhat.com>
2016-07-27 17:24:19 +0800

    A few db tweak

--- caselink/models.py
+++ caselink/models.py
@@ -26,7 +26,7 @@ def test_pattern_match(pattern, casename):
 
 class Error(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
-    message = models.CharField(max_length=255, blank=True)
+    message = models.CharField(max_length=65535, blank=True)
     def __str__(self):
         return self.id + ":" + self.message
 
@@ -175,7 +175,7 @@ class AutoCase(models.Model):
 class CaseLink(models.Model):
     workitem = models.ForeignKey(WorkItem, on_delete=models.PROTECT, related_name='caselinks')
     autocases = models.ManyToManyField(AutoCase, blank=True, related_name='caselinks')
-    autocase_pattern = models.CharField(max_length=255)
+    autocase_pattern = models.CharField(max_length=65535)
     framework = models.ForeignKey(Framework, on_delete=models.PROTECT, null=True,
                                   related_name='caselinks')
     errors = models.ManyToManyField(Error, blank=True, related_name='caselinks')
@@ -262,7 +262,7 @@ class AutoCaseFailure(models.Model):
                                   related_name='autocase_failures')
     bug = models.ForeignKey('Bug', related_name='autocase_failures', blank=True, null=True)
     failure_regex = models.CharField(max_length=65535)
-    autocase_pattern = models.CharField(max_length=255)
+    autocase_pattern = models.CharField(max_length=65535)
 
     class Meta:
         unique_together = ("failure_regex", "autocase_pattern",)
