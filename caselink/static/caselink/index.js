d7791d2211f1612b839b42b6d6231255bea809f5
Kairui Song <kasong@redhat.com>
2016-08-11 12:54:01 +0800

    Update js fix some table error.

--- caselink/static/caselink/index.js
+++ caselink/static/caselink/index.js
@@ -2,9 +2,10 @@ function prettier(data){
     var regex = /^\s*$/ ;
     if(!data)
         return "---"
-    if(data instanceof Array)
+    if(data instanceof Array){
         if(data.length === 0)
             return "---"
+    }
     if(typeof data === 'string'){
         if(data.match(regex))
             return "---"
