Hao-Liu/caselink

commit df0d831d4c65fa2b5f0a75ca7054759f965f27ab

Fix for python 3

caselink/tasks/polarion.py
@@ -21,10 +21,6 @@ MANUAL_SPACE = 'Virt-LibvirtQE'
 DEFAULT_COMPONENT = 'libvirt'
 
 
-class literal(unicode):
-    pass
-
-
 def load_polarion(project, space, load_automation=False):
     """
     Load all Manual cases with given project and spave, return a dictionary,
@@ -42,8 +38,8 @@ def load_polarion(project, space, load_automation=False):
         return all_cases
 
     obj = OrderedDict([
-        ('project', literal(project)),
-        ('space', literal(space)),
+        ('project', project),
+        ('space', space),
         ('documents', OrderedDict()),
     ])
     docs = Document.get_documents(
@@ -53,8 +49,8 @@ def load_polarion(project, space, load_automation=False):
             current_task.update_state(state='PROGRESS',
                                       meta={'current': doc_idx, 'total': len(docs)})
         obj_doc = OrderedDict([
-            ('title', literal(doc.title)),
-            ('type', literal(doc.type)),
+            ('title', doc.title),
+            ('type', doc.type),
             ('project', project),
             ('work_items', OrderedDict()),
             ('updated', doc.updated),
@@ -62,13 +58,13 @@ def load_polarion(project, space, load_automation=False):
         wis = doc.get_work_items(None, True, fields=workitem_fields_to_load)
         for wi_idx, wi in enumerate(wis):
             obj_wi = OrderedDict([
-                ('title', literal(wi.title)),
-                ('type', literal(wi.type)),
+                ('title', wi.title),
+                ('type', wi.type),
                 ('project', project),
                 ('updated', wi.updated),
             ])
-            obj_doc['work_items'][literal(wi.work_item_id)] = obj_wi
-        obj['documents'][literal(doc.document_id)] = obj_doc
+            obj_doc['work_items'][wi.work_item_id] = obj_wi
+        obj['documents'][doc.document_id] = obj_doc
     cases = flatten_cases(obj)
     if load_automation:
         sync_automation(cases, mode="poll")

setup_db.py
@@ -36,6 +36,4 @@ def run():
     init_error_checking()
 
 if __name__ == '__main__':
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
     run()

caselink/settings.py
@@ -147,5 +147,5 @@ CASELINK_MAITAI = {
 try:
     from caselink.settings_instance import *
 except ImportError:
-    print "Instance setting not found."
+    print("Instance setting not found.")
 

caselink/tasks/common.py
@@ -283,7 +283,7 @@ def _load_bug(failures):
         case_updating = failure.get('case-updating', None)
         workitems = failure.get('workitems', None)
         if (bug_id and case_updating) or (not bug_id and not case_updating):
-            print "Bad entry: " + str(failure)
+            print("Bad entry: " + str(failure))
             continue
 
         bug = None

caselink/views/control.py
@@ -182,7 +182,6 @@ def upload(request):
 
 
 def create_maitai_request(request, workitem_id=None):
-    print settings.CASELINK_MAITAI
     if not settings.CASELINK_MAITAI['ENABLE']:
         reason = (
             settings.CASELINK_MAITAI['REASON'] or 'Maitai disabled, please contact the admin.')

commit 198b5ed61249705d855fad7dc7cf0a8a848f301f

Fix problem introduced by df0d831d

caselink/tasks/polarion.py
@@ -21,6 +21,14 @@ MANUAL_SPACE = 'Virt-LibvirtQE'
 DEFAULT_COMPONENT = 'libvirt'
 
 
+try:
+    class literal(unicode):
+        pass
+except NameError:
+    class literal(str):
+        pass
+
+
 def load_polarion(project, space, load_automation=False):
     """
     Load all Manual cases with given project and spave, return a dictionary,
@@ -38,8 +46,8 @@ def load_polarion(project, space, load_automation=False):
         return all_cases
 
     obj = OrderedDict([
-        ('project', project),
-        ('space', space),
+        ('project', literal(project)),
+        ('space', literal(space)),
         ('documents', OrderedDict()),
     ])
     docs = Document.get_documents(
@@ -49,8 +57,8 @@ def load_polarion(project, space, load_automation=False):
             current_task.update_state(state='PROGRESS',
                                       meta={'current': doc_idx, 'total': len(docs)})
         obj_doc = OrderedDict([
-            ('title', doc.title),
-            ('type', doc.type),
+            ('title', literal(doc.title)),
+            ('type', literal(doc.type)),
             ('project', project),
             ('work_items', OrderedDict()),
             ('updated', doc.updated),
@@ -58,13 +66,13 @@ def load_polarion(project, space, load_automation=False):
         wis = doc.get_work_items(None, True, fields=workitem_fields_to_load)
         for wi_idx, wi in enumerate(wis):
             obj_wi = OrderedDict([
-                ('title', wi.title),
-                ('type', wi.type),
+                ('title', literal(wi.title)),
+                ('type', literal(wi.type)),
                 ('project', project),
                 ('updated', wi.updated),
             ])
-            obj_doc['work_items'][wi.work_item_id] = obj_wi
-        obj['documents'][doc.document_id] = obj_doc
+            obj_doc['work_items'][literal(wi.work_item_id)] = obj_wi
+        obj['documents'][literal(doc.document_id)] = obj_doc
     cases = flatten_cases(obj)
     if load_automation:
         sync_automation(cases, mode="poll")

setup_db.py
@@ -36,4 +36,7 @@ def run():
     init_error_checking()
 
 if __name__ == '__main__':
+    if sys.version_info < (3, 0):
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
     run()