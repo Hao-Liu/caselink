e8c0e53514586512b0d17ec712d8146a0b240fc7
Kairui Song <kasong@redhat.com>
2016-10-12 16:46:29 +0800

    Use timestamp to speed up polarion fetching, and restruct code.
    After we introduced timestamp column, we can skip up-to-dated workitems
    when fetching from polarion.

--- caselink/tasks/polarion.py
+++ caselink/tasks/polarion.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+from django.core.exceptions import ObjectDoesNotExist
+import pytz
 
 from collections import OrderedDict
 try:
@@ -30,13 +32,15 @@ except NameError:
         pass
 
 
-def load_polarion(project, space, load_automation=False):
+def load_polarion(project, space):
     """
     Load all Manual cases with given project and spave, return a dictionary,
     keys are workitem id, values are dicts presenting workitem attributes.
     """
-    workitem_fields_to_load = ['work_item_id', 'type', 'title', 'updated']
+    # If called as a celery task...
     direct_call = current_task.request.id is None
+
+    utc = pytz.UTC
 
     def flatten_cases(docs):
         all_cases = {}
@@ -58,98 +62,85 @@ def load_polarion(project, space, load_automation=False):
             ('type', literal(doc.type)),
             ('project', project),
             ('work_items', OrderedDict()),
-            ('updated', doc.updated),
+            ('updated', utc.localize(doc.updated)),
         ])
-        wis = doc.get_work_items(None, True, fields=workitem_fields_to_load)
+        wis = doc.get_work_items(None, True, fields=['work_item_id', 'type', 'title', 'updated'])
         for wi_idx, wi in enumerate(wis):
             obj_wi = OrderedDict([
                 ('title', literal(wi.title)),
                 ('type', literal(wi.type)),
                 ('project', project),
-                ('updated', wi.updated),
+                ('updated', utc.localize(wi.updated)),
             ])
             obj_doc['work_items'][literal(wi.work_item_id)] = obj_wi
         doc_dict[literal(doc.document_id)] = obj_doc
     cases = flatten_cases(doc_dict)
-    if load_automation:
-        sync_automation(cases, mode="poll")
     return cases
 
 
-def sync_automation(workitem_dict, mode="poll"):
+def get_automation_of_wi(wi_id):
     """
-    Automation attribute is a custom column and we fetch it sperately.
-    :param workitem_dict: A dictionary contains all workitems need to sync.
-        eg. {'RHEL-1234': {'automation': False}}
-    :param mode: "poll" or "push", poll will override attributes in given dict,
-        push will override polarion
+    Get the automation status of a workitem.
     """
-    # TODO: Find a way to fetch all needed attributes all in once,
-    direct_call = current_task.request.id is None
-    current, total = 0, len(workitem_dict.keys())
-    for wi_id, wi in workitem_dict.items():
-        current += 1
-        if not direct_call:
-            current_task.update_state(state='Fetching automation status',
-                                      meta={'current': current, 'total': total})
-        polarion_wi = _WorkItem(project_id=PROJECT, work_item_id=wi_id)
-        if mode == "poll":
-            try:
-                for custom in polarion_wi._suds_object.customFields.Custom:
-                    if custom.key == 'caseautomation':
-                        wi['automation'] = custom.value.id
-            except AttributeError:
-                # Skip heading / case with no automation attribute
-                pass
-            # If automation is not set, set to not automated.
-            wi.setdefault('automation', 'notautomated')
-
-        elif mode == "push":
-            raise RuntimeError('push disabled')
-            session = _WorkItem.session
-            session.tx_begin()
-            if wi['automation'] in ['notautomated', None]: #TODO: What?
-                value = EnumOptionId(enum_id='automated')
-                polarion_wi._set_custom_field('caseautomation', value._suds_object)
-            session.tx_commit()
+    ret = 'notautomated'
+    polarion_wi = _WorkItem(project_id=PROJECT, work_item_id=wi_id)
+    try:
+        for custom in polarion_wi._suds_object.customFields.Custom:
+            if custom.key == 'caseautomation':
+                ret = custom.value.id
+    except AttributeError:
+        # Skip heading / case with no automation attribute
+        pass
+    return ret
 
 
 @shared_task
 def sync_with_polarion():
+    """
+    Main task, fetch workitems from polarion.
+    """
     if not PYLARION_INSTALLED:
         return "Pylarion not installed"
     if not settings.CASELINK_POLARION['ENABLE']:
         return settings.CASELINK_POLARION['REASON']
-    current_polarion_workitems = load_polarion(PROJECT, MANUAL_SPACE, load_automation=True)
-    current_caselink_workitems = models.WorkItem.objects.all()
 
+    current_polarion_workitems = load_polarion(PROJECT, MANUAL_SPACE)
+    current_caselink_workitems = models.WorkItem.objects.all()
     polarion_set = set(current_polarion_workitems.keys())
     caselink_set = set(current_caselink_workitems.values_list('id', flat=True))
 
-    new_wi = polarion_set - caselink_set
-    deleted_wi = caselink_set - polarion_set
-    existing_wi = caselink_set & polarion_set
-    updated_wi = set()
+    updating_set = polarion_set & caselink_set
+    deleting_set = caselink_set - polarion_set
+    creating_set = polarion_set - caselink_set
 
     direct_call = current_task.request.id is None
     if not direct_call:
         current_task.update_state(state='Updating database.')
 
-    for wi_id in new_wi:
+    # Ignore workitems with an up-to-dated timestamp
+    for wi_id in updating_set.copy():
+        workitem = models.WorkItem.objects.get(id=wi_id)
+        if workitem.updated == current_polarion_workitems[wi_id]['updated']:
+            updating_set.discard(wi_id)
+
+    # Fetch automation info information
+    for wi_id in creating_set | updating_set:
+        current_polarion_workitems[wi_id]['automation'] = get_automation_of_wi(wi_id)
+
+    for wi_id in creating_set:
         with transaction.atomic():
             wi = current_polarion_workitems[wi_id]
             workitem = models.WorkItem(
-                #TODO: automation
                 id=wi_id,
                 title=wi['title'],
                 type=wi['type'],
+                updated=wi['updated'],
+                automation=wi.get('automation', 'notautomated')
             )
 
             workitem.project, created = models.Project.objects.get_or_create(name=wi['project'])
 
-            if 'automation' in wi:
-                workitem.automation = 'automated' if wi['automation'] == 'automated' else 'notautomated'
-
+            # Commit db changes, or there could be a Integrity error.
             workitem.save()
 
             for doc_id in wi['documents']:
@@ -176,35 +167,36 @@ def sync_with_polarion():
 
             workitem.save()
 
-    for wi_id in deleted_wi:
+    for wi_id in deleting_set:
         models.WorkItem.objects.get(id=wi_id).mark_deleted()
 
-    for wi_id in existing_wi:
+    for wi_id in updating_set:
         with transaction.atomic():
-            wi = models.WorkItem.objects.get(id=wi_id)
-            wi.mark_notdeleted()
+            wi = current_polarion_workitems[wi_id]
+            workitem = models.WorkItem.objects.get(id=wi_id)
 
-            wi.documents.clear()
-            for doc_id in current_polarion_workitems[wi_id]['documents']:
+            # In case a old deleted case show up again in polarion.
+            workitem.mark_notdeleted()
+
+            workitem.documents.clear()
+            for doc_id in wi['documents']:
                 doc, created = models.Document.objects.get_or_create(id=doc_id)
                 if created:
                     doc.title = doc_id
                     doc.component = models.Component.objects.get_or_create(name=DEFAULT_COMPONENT)
-                doc.workitems.add(wi)
+                doc.workitems.add(workitem)
                 doc.save()
 
-            if wi.title != current_polarion_workitems[wi_id]['title']:
-                wi.title = current_polarion_workitems[wi_id]['title']
-                wi.save()
-                updated_wi.add(wi_id)
-            if 'automation' in current_polarion_workitems[wi_id].keys():
-                if wi.automation != current_polarion_workitems[wi_id]['automation']:
-                    wi.automation = current_polarion_workitems[wi_id]['automation']
-                    wi.save()
-                    updated_wi.add(wi_id)
+            workitem.title = wi['title']
+            workitem.automation = wi.get('automation', workitem.automation)
+            workitem.updated = wi['updated']
+
+            #TODO: Trigger a maitai job by checking automation and history.
+
+            workitem.save()
 
     return (
-        "Created: " + ', '.join(new_wi) + "\n" +
-        "Deleted: " + ', '.join(deleted_wi) + "\n" +
-        "Updated: " + ', '.join(updated_wi)
+        "Created: " + ', '.join(creating_set) + "\n" +
+        "Deleted: " + ', '.join(deleting_set) + "\n" +
+        "Updated: " + ', '.join(updating_set)
     )
