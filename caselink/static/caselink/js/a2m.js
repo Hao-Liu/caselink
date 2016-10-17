ecc058f6a53da7a714ebaab48d9c7a8c6912c4b4
ryncsn <ryncsn@gmail.com>
2016-10-17 23:24:30 +0800

    Split js and html.

---
+++ caselink/static/caselink/js/a2m.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+  var table = $('#sortable-table').DataSearchTable( {
+    "ajax": "data?type=a2m",
+    "iDisplayLength": 20,
+    "bAutoWidth": false,
+    "selectorColumns": [
+      {
+        column: 'Component',
+        render: prettier,
+      },
+      {
+        column: 'Framework',
+        render: prettier,
+      },
+      {
+        column: 'Documents',
+        render: prettier,
+      },
+      {
+        column: 'PR',
+        render: prettier,
+      },
+      {
+        column: 'Errors',
+        render: prettier,
+      }
+    ],
+    "columns": [
+      { "data": "case" },
+      {
+        "data": "polarion",
+        "render": function(data) {
+          link = ""
+          for (i in data){
+            polarion_id = data[i];
+            link += '<a href="https://polarion.engineering.redhat.com/polarion/#/project/RedHatEnterpriseLinux7/workitem?id='+polarion_id+'">'+polarion_id+'</a><br>';
+          }
+          return link;
+        }
+      },
+      {
+        "data": "title",
+        "render": function(data){
+          return prettier(data.join('<br>'));
+        }
+      },
+      {
+        "data": "documents",
+        "render": function(data){
+          return prettier(data.join('<br>'));
+        }
+      },
+      {
+        "data": "components",
+        "render": function(data){
+          return prettier(data.join('<br>'));
+        }
+      },
+      {
+        "data": "framework",
+      },
+      {
+        "data": "pr",
+        "render":  prettier
+      },
+      {
+        "data": "errors",
+        "render": function(data){
+          return prettier(data.join('<br>'));
+        }
+      },
+    ],
+    "createdRow": function (row, data, index){
+      if (data.errors.length > 0) {
+        $('td', row).eq(0).addClass('errors');
+      }
+    }
+  });
+});
+
