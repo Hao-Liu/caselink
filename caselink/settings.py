a08f2869dd889f6a7836af2f8014d6133d4bd1d8
ryncsn <ryncsn@gmail.com>
2016-12-11 19:36:07 +0800

    Use sqlite3 as default database backend.
    Use setting_instance.py to store config for production, setting.py
    should provide config for devel.

--- caselink/settings.py
+++ caselink/settings.py
@@ -110,12 +110,8 @@ CELERYBEAT_SCHEDULE = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
